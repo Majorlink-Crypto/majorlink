12bc9d322fd74d02b5613a4d3940cd7c540b8d53
caprule <folabas2@gmail.com>
2026-07-13 11:10:42 +0000

    feat: initialize application routing and global Tailwind CSS styles

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   }, [])
 
   return (
-    <div className='font-aeonikmedium h-full'>
+    <div className='font-aeonikmedium'>
       <BrowserRouter>
       <ToastContainer />
        <Routes>
